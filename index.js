71438a26c655df3d89eb3950c41ef57ec1d91ec8
Philipp Mulfinger <info@phil-splash.de>
2023-10-23 11:19:51 +0200

    Added Files Routes, Controllers, Models

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ app.use("/", (req, res) => {
   res.send("Hello Marta, Piotr, Soydan, Phil!");
 });
 
+/* 
+
+ALL ROUTES
+
+Home Route ("/")
+"/*" , 404
+
+Authentication Route ("/auth")
+"/signup", post, createNewOne
+"/signin", post
+
+User Route ("/user")
+"/:id", get, delete
+"/:id/cart", get, delete, post
+"/:id/wishlist", get, delete, post
+
+*/
+
 app.listen(PORT, (req, res) => {
   console.log(`Example app listening on http://localhost:${PORT}`);
 });
